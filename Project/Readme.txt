83716d4c6afff57a772c3bdef7a07a2562e6859b
bmk777 <47337636+bmk777@users.noreply.github.com>
2021-10-29 17:15:21 -0400

    Add files via upload

---
+++ Project/Readme.txt
@@ -0,0 +1,20 @@
+To run the client application
+
+docker pull bmk77/workingproject:1
+docker run -it workingproject:1
+Follow the on screen instructions to continue
+
+Plan to connect to GCP
+Extend terminal functionality to replicate running the program in the GCP terminal
+For file location
+	User selects case one of the menu.
+	//Execute gcloud $some authentication protocol
+	//Create a temporary bucket and store the file location inside
+	//Print out command to access file locatin
+
+For word
+	User selects case two of the menu
+	//Check the user is authenticated
+	//navigate to file location
+	//implement sort/find scheme (inverted)
+	//return data: Frequence, ect..
--- Project/run.java
+++ Project/run.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 public class run {
     
@@ -6,28 +5,33 @@ public class run {
         
         Scanner in = new Scanner(System.in);
         // Display the menu
-        System.out.println("1\t Apache Hadoop");
-        System.out.println("2\t Apache Sparch");
-        System.out.println("3\t Jupyter Notebook");
-        System.out.println("4\t SonarQube and SonarScanner");
+        System.out.println("1\t Locate a file");
+        System.out.println("2\t Search word");
+ 
 
-        
+        //USE SH FILE TO MAKE A FOR LOOP TO CONTINUSOUSLY READ USER INPUT
+        //This is good shithttps://stackoverflow.com/questions/39496017/iterate-in-run-command-in-dockerfile
+       // https://hpc-wiki.info/hpc/Sh-file so is this
+       // https://www.cyberciti.biz/faq/bash-for-loop/#Examples so isx this (examples)
+
         
         //Get user's choice
         int choice = 0;
+        String location;
+        String word;
         while(choice != -1){
             System.out.println("Please enter your choice:");
-            choice=in.nextInt();
+            choice = in.nextInt();
         //Display the title of the chosen module
         switch (choice) {
-            case 1: System.out.println("Apache Hadoop"); 
-	   break;
-            case 2: System.out.println("Apache Sparch");
+            case 1: System.out.println("Locate a file"); 
+                    //System.out.println("Please enter your file choice:");
+                    //location = in.nextLine();
+	                break;
+            case 2: System.out.println("Search word");
+                    //System.out.println("Please enter your word choice:");
+                    //word = in.nextLine();
                     break;
-            case 3: System.out.println("Jupyter Notebook"); 
-                    break;
-            case 4: System.out.println("SonarQube and SonarScanner"); 
-                     break;
             default: System.out.println("Invalid choice");
             }
         }//end of switch
