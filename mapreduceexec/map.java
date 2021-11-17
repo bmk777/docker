d4ea01d25412ed231ebacc833e9cb77447829c1d
bmk777 <bmk77@pitt.edu>
2021-11-17 16:46:27 -0500

    hooray!

---
+++ mapreduceexec/map.java
@@ -0,0 +1,9 @@
+public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString());
+      while (itr.hasMoreTokens()) {
+        word.set(itr.nextToken());
+        context.write(word, one);
+      }
+    }
+  }
